7fc9414ba0021f05c2cea66a2cb8e7d6806d399a
askari0102 <48948871+askari0102@users.noreply.github.com>
2026-04-17 19:21:10 +0700

    Update outputs.tf

--- Week 5/Terraform/outputs.tf
+++ Week 5/Terraform/outputs.tf
@@ -1,11 +1,21 @@
-# Ngambil IP Gateway Server
+# Ngambil IP Publik Gateway Server
 output "gateway_ip" {
   value       = aws_eip.gateway_eip.public_ip
 }
 
-# Ngambil IP Appserver
+# Ngambil IP Publik Appserver
 output "app_server_ip" {
   value       = aws_eip.appserver_eip.public_ip
+}
+
+# Ambil IP Private Gateway
+output "gateway_private_ip" {
+  value = aws_instance.gateway_server.private_ip
+}
+
+# Ambil IP Private Appserver
+output "app_private_ip" {
+  value = aws_instance.app_server.private_ip
 }
 
 output "ssh_key_pem" {
