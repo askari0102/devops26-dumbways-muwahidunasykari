3475bd23db9d9767bbe211178ee9ea7f8a890660
askari0102 <48948871+askari0102@users.noreply.github.com>
2026-04-17 18:21:15 +0700

    Create ansible-inventory.tf

---
+++ Week 5/Terraform/ansible-inventory.tf
@@ -0,0 +1,15 @@
+resource "local_file" "ansible_inventory" {
+  content = <<EOT
+[gateway]
+gateway_server ansible_host=${aws_eip.gateway_eip.public_ip}
+
+[app]
+app_server ansible_host=${aws_eip.appserver_eip.public_ip}
+
+[all:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=../terraform/deployer-key.pem
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+EOT
+  filename = "${path.module}/../ansible/Inventory"
+}
