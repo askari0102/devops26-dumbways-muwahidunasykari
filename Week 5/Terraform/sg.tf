7a2d2fa4501799a951d22a0fdf61eb8ddfd1e32d
askari0102 <48948871+askari0102@users.noreply.github.com>
2026-04-19 20:48:22 +0700

    Update sg.tf

--- Week 5/Terraform/sg.tf
+++ Week 5/Terraform/sg.tf
@@ -10,8 +10,14 @@ resource "aws_security_group" "sg_app" {
   tags   = { Name = "sg-app" }
 }
 
+resource "aws_security_group" "sg_monitoring" {
+  name   = "sg_monitoring"
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "sg-monitoring" }
+}
+
 # Inbound Gateway SSH + HTTP
-resource "aws_security_group_rule" "allow_http_inbound" {
+resource "aws_security_group_rule" "allow_http_gateway" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
@@ -37,6 +43,35 @@ resource "aws_security_group_rule" "allow_ssh_app" {
   cidr_blocks       = ["0.0.0.0/0"] # Terima semua IP untuk SSH 
   security_group_id = aws_security_group.sg_app.id
 }
+
+# Rule SSH & Web Monitoring
+resource "aws_security_group_rule" "allow_ssh_monitoring" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_monitoring.id
+}
+
+resource "aws_security_group_rule" "allow_prometheus_web" {
+  type              = "ingress"
+  from_port         = 9090
+  to_port           = 9090
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.sg_gateway_db.id
+  security_group_id = aws_security_group.sg_monitoring.id
+}
+
+resource "aws_security_group_rule" "allow_grafana_web" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.sg_gateway_db.id
+  security_group_id = aws_security_group.sg_monitoring.id
+}
+
 # Integrasi 
 resource "aws_security_group_rule" "allow_db_from_app" {
   type                     = "ingress"
@@ -82,4 +117,73 @@ resource "aws_security_group_rule" "app_outbound" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg_app.id
+}
+
+resource "aws_security_group_rule" "monitoring_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_monitoring.id
+}
+
+# Izin Node Exporter untuk Monitoring
+resource "aws_security_group_rule" "allow_node_exporter_self" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_monitoring.id # Dari dirinya sendiri
+  security_group_id        = aws_security_group.sg_monitoring.id
+}
+# - Di Server Gateway
+resource "aws_security_group_rule" "allow_node_exporter_gateway" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_monitoring.id
+  security_group_id        = aws_security_group.sg_gateway_db.id
+}
+
+# - Di Server Appserver
+resource "aws_security_group_rule" "allow_node_exporter_app" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_monitoring.id
+  security_group_id        = aws_security_group.sg_app.id
+}
+
+# Izinkan Gateway untuk akses Node Exporter 
+# 1. Izinkan Gateway akses port 9100 di Server App
+resource "aws_security_group_rule" "allow_node_exporter_app_from_gateway" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_gateway_db.id 
+  security_group_id        = aws_security_group.sg_app.id
+}
+
+# 2. Izinkan Gateway akses port 9100 di Server Monitoring
+resource "aws_security_group_rule" "allow_node_exporter_monitor_from_gateway" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_gateway_db.id 
+  security_group_id        = aws_security_group.sg_monitoring.id
+}
+
+# 3. Izinkan Gateway akses port 9100 ke DIRINYA SENDIRI (PENTING!)
+resource "aws_security_group_rule" "allow_node_exporter_gateway_self" {
+  type                     = "ingress"
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.sg_gateway_db.id
+  security_group_id        = aws_security_group.sg_gateway_db.id
 }
